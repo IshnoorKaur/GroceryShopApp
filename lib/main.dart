0606a8a6772302fd14fe0daf1c9a473f09fc43c9
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2025-01-08 23:01:45 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
+// Importing the Flutter material library for UI components
 import 'package:flutter/material.dart';
-import 'pages/intro_page.dart'
 
+// Importing the IntroPage widget from the pages folder
+import 'pages/intro_page.dart';
+
+// Entry point of the application
 void main() {
+  // Runs the MyApp widget as the root of the application
   runApp(const MyApp());
 }
 
+// The main application widget
 class MyApp extends StatelessWidget {
+  // A constant constructor for the MyApp class
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(actions: 
-    debugShowCheckedModeBanner:false,
-    home: IntroPage(),
+    // Returns the MaterialApp widget, which sets up the app's structure
+    return const MaterialApp(
+      // Hides the debug banner in the app
+      debugShowCheckedModeBanner: false,
+      
+      // Sets the IntroPage widget as the home screen
+      home: IntroPage(),
     );
   }
 }
