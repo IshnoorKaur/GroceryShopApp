609f9609450bf6fc708835116bb74864d4bb7dcf
Ishnoor Kaur <119340690+IshnoorKaur@users.noreply.github.com>
2025-07-08 20:23:34 -0400

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+final List<Product> sampleProducts = [
+  Product(name: 'Apples', price: 2.99),
+  Product(name: 'Bananas', price: 1.49),
+  Product(name: 'Carrots', price: 1.99),
+  Product(name: 'Bread', price: 3.49),
+];
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
